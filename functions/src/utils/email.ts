9b3dc8184db3e8fde7e7e803e414cdb702fc857b
Richmond Glenn Viloria <sunikhedge@gmail.com>
2025-09-03 16:13:54 +0800

    fixed env issues

--- functions/src/utils/email.ts
+++ functions/src/utils/email.ts
@@ -2,13 +2,33 @@ import { Resend } from "resend";
 import dotenv from "dotenv";
 dotenv.config();
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+let sendEmailImpl: (to: string, subject: string, html: string) => Promise<void>;
 
-export const sendEmail = async (to: string, subject: string, html: string) => {
-  await resend.emails.send({
-    from: "Moldify Auto-email <onboarding@resend.dev>",
-    to: [to],
-    subject: subject,
-    html: html,
+if (process.env.NODE_ENV === "test" || process.env.USE_MAILHOG === "true") {
+  const transporter = require('nodemailer').createTransport({
+    host: 'localhost',
+    port: 1025, // MailHog default SMTP port
+    secure: false,
+    auth: null,
   });
-};
+  sendEmailImpl = async (to: string, subject: string, html: string) => {
+    await transporter.sendMail({
+      from: "Moldify Auto-email <onboarding@resend.dev>",
+      to: [to],
+      subject: subject,
+      html: html,
+    });
+  };
+} else {
+  const resend = new Resend(process.env.RESEND_API_KEY);
+  sendEmailImpl = async (to: string, subject: string, html: string) => {
+    await resend.emails.send({
+      from: "Moldify Auto-email <onboarding@resend.dev>",
+      to: [to],
+      subject: subject,
+      html: html,
+    });
+  };
+}
+
+export const sendEmail = sendEmailImpl;
