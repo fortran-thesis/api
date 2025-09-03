7263050643df8f9b8bfaf97abc9a18b7fa4e1b1a
Richmond Glenn Viloria <sunikhedge@gmail.com>
2025-09-03 15:57:20 +0800

    fixed firebase.ts

--- functions/src/configs/firebase.ts
+++ functions/src/configs/firebase.ts
@@ -1,5 +1,4 @@
 import { initializeApp, cert, getApps, getApp } from "firebase-admin/app";
-import serviceAccount from "./firebase-config.json";
 
 let firebaseConfig: any = {};
 
@@ -12,7 +11,5 @@ if (!process.env.FIRESTORE_EMULATOR_HOST) {
 
 // Initialize Firebase Admin SDK with service account
 export const firebase = !getApps().length
-  ? initializeApp({
-      credential: cert(serviceAccount as any),
-    })
+  ? initializeApp(firebaseConfig)
   : getApp();
